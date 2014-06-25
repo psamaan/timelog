db8a5ed0b574050d57ce7c463c4552896c29ce56
psamaan <firestone.light@gmail.com>
2014-06-25 12:02:36 -0700

    auto clock out tasks now tested and is working. schedule is set every day at 11:55 PM.

--- app/tasks.js
+++ app/tasks.js
@@ -13,15 +13,19 @@ module.exports = function() {
     'use strict';
 
     var dailyRule0 = new schedule.RecurrenceRule();
-    dailyRule0.hour = 16;
-    dailyRule0.minute = 56;
+    dailyRule0.hour = 23;
+    dailyRule0.minute = 55;
 
     var clockOutAll = schedule.scheduleJob(dailyRule0, function () {
 
         console.log("auto clock out started");
 
-        Log.collection.find({}).each(function (dayLog) {
-            console.log(dayLog);
+        var stream = Log.find().stream();
+
+        stream.on('data', function (dayLog) {
+            // do something with the mongoose document
+
+        // Log.collection.find().toArray().forEach(function (dayLog) {
                     var hasClockOut = false;
                     for (var l = 0 ; l < dayLog.logEntries.length ; l++) {
                         if (dayLog.logEntries[l].action === "clock out") hasClockOut = true;
@@ -42,24 +46,28 @@ module.exports = function() {
                             lat: "33.63"
                         };
                         dayLog.logEntries.push(clockLog);
-                        dayLog.worked_minutes += ((Date.now() - dayLog.logEntries[clockInIndex].datetime) / 60000 - dayLog.lunch_minutes);
                         var query = {};
                         query.fullname = dayLog.name;
                         User.findOne(query, function (err, user) {
                             if (err) throw err;
-                            if (dayLog.worked_minutes > user.hours * 60) dayLog.overtime_minutes = dayLog.worked_minutes - user.hours * 60;
                             dayLog.mComment = "System Generated Comment: Employee did not clock out manually, record was closed automatically at the end of the day. Worked hours and overtime should be reviewed by " + user.manager;
                             dayLog.save(function (err) {
                                 if (err) throw err;
                                 user.state = "off";
                                 user.save(function (err) {
                                     if (err) throw err;
-                                    //all worked, send changed user
-                                    console.log("success closing open clock");
+                                    //all worked, log success
+                                    console.log("success closing open clock of " + user.fullname + " on " + clockLog.datetime);
                                 });
                             });
                         });
                     }
-        });
+    }).on('error', function (err) {
+        // handle the error
+            throw err;
+    }).on('close', function () {
+        // the stream is closed
+            console.log("done iterating through log");
+    });
     });
 };
